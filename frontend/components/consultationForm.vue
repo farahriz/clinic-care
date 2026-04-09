6b2cec16ff9e69853cbc382325065a30fb3cc323
Farah R <41633573+farahriz@users.noreply.github.com>
2026-04-10 00:11:06 +0800

    feat: create basic form for inputing consultation note details

---
+++ frontend/components/consultationForm.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-form>
+        <v-text-field label="Patient" v-model="formData.patient"></v-text-field>
+        <v-text-field label="Consultation Date" v-model="formData.consultDate"></v-text-field>
+        <v-autocomplete label="Diagnosis" v-model="formData.code"></v-autocomplete>
+        <v-textarea label="Description" v-model="formData.description"></v-textarea>
+    </v-form>
+</template>
+
+<script setup lang="ts">
+const formData = ref({
+    id: null,
+    code: "",
+    patient: "",
+    consultDate: "",
+    description: ""
+})
+</script>
