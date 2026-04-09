0311bc44cb6ea8462cec4b6c2bb3e77066c7b205
Farah R <41633573+farahriz@users.noreply.github.com>
2026-04-10 01:27:06 +0800

    feat: pseudo-code scaping file for getting codes to populate database

---
+++ api/scrape.py
@@ -0,0 +1,25 @@
+# beautiful soup
+
+website_url = "https://www.icd10data.com/ICD10CM/"
+
+target_page_group = "A00-B99"
+
+# find link to group page
+code_groups = ["A00-A09", "A15-A19", "A20-A28", "A30-A49", "A50-A64", "A65-A69"]
+
+
+# for each group page, find link to individaul code
+code_subgroups = ["A00", "A01", "A02", "A03", "A04", "A05", "A06", "A07", "A08", "A09"]
+
+# go to individual code page
+
+#find section with code
+
+# Get code
+# e.g. code = "A00"
+
+# Get name
+# e.g. name = "Cholera"
+
+
+#once there are no more codes, go back
