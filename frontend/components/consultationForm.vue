2afaa064e0e57f2f339966d212a43ead104d95c3
Farah R <41633573+farahriz@users.noreply.github.com>
2026-04-10 01:00:25 +0800

    feat: make form inputs disabled via prop for viewing data

--- frontend/components/consultationForm.vue
+++ frontend/components/consultationForm.vue
@@ -1,18 +1,27 @@
 <template>
     <v-form>
-        <v-text-field label="Patient" v-model="formData.patient"></v-text-field>
-        <v-text-field label="Consultation Date" v-model="formData.consultDate"></v-text-field>
-        <v-autocomplete label="Diagnosis" v-model="formData.code"></v-autocomplete>
-        <v-textarea label="Description" v-model="formData.description"></v-textarea>
+        <v-text-field :disabled="readOnly" label="Patient" v-model="formData.patient"></v-text-field>
+        <v-text-field  :disabled="readOnly" label="Consultation Date" v-model="formData.consultDate"></v-text-field>
+        <v-autocomplete :disabled="readOnly" label="Diagnosis" v-model="formData.code"></v-autocomplete>
+        <v-textarea :disabled="readOnly" label="Description" v-model="formData.description"></v-textarea>
     </v-form>
 </template>
 
 <script setup lang="ts">
-const formData = ref({
-    id: null,
-    code: "",
-    patient: "",
-    consultDate: "",
-    description: ""
+const formData = defineModel('consultNoteData')
+
+const props = defineProps({
+    readOnly: {
+        type: Boolean,
+        default: false
+    }
 })
+
+// const formData = ref({
+//     id: null,
+//     code: "",
+//     patient: "",
+//     consultDate: "",
+//     description: ""
+// })
 </script>
