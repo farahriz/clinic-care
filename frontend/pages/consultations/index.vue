01dcdb7d327c58ca305456f3e13e871c52ed38cc
Farah R <41633573+farahriz@users.noreply.github.com>
2026-04-10 00:11:32 +0800

    feat: populate index page with welcome message, add placeholder table to consulation list page

--- frontend/pages/consultations/index.vue
+++ frontend/pages/consultations/index.vue
@@ -1,6 +1,15 @@
 <template>
     <h1>Consultations</h1>
+    <v-btn color="primary"  text="Add new consulation" prepend-icon="mdi-plus">
+    </v-btn>
+
+    <ConsultationForm></ConsultationForm>
+
+    <v-data-table class="mt-5"></v-data-table>
 </template>
 
 <script setup lang="ts">
+import ConsultationForm from '~/components/consultationForm.vue';
+
+const consulations = ref([])
 </script>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,6 +1,7 @@
 <template>
-    <h1>This is a page header</h1>
-    <v-btn color="primary" text="test"></v-btn>
+        <h1>Welcome to Clinic Care</h1>
+        <v-btn color="primary" text="Manage consult notes" class="mr-3" :to="'/consultations'" ></v-btn>
+        <v-btn color="secondary" text="View diagnoses codes" :to="'/diagnoses'"></v-btn>
 </template>
 
 <script lang="ts">
