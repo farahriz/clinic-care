312c7b97c4a1184095140392d20512cc6fa30298
Farah R <41633573+farahriz@users.noreply.github.com>
2026-04-13 00:59:28 +0800

    feat: Format code info into simple data table

--- frontend/pages/diagnoses/index.vue
+++ frontend/pages/diagnoses/index.vue
@@ -1,11 +1,20 @@
 <template>
-    <h1>List of Diagnosis Codes</h1>
-    {{ diagnoses }}
+    <v-card title="List of Diagnosis Codes">
+        <template v-slot:text>
+            <v-text-field
+                v-model="search"
+                label="Search"
+                prepend-inner-icon="mdi-magnify"
+                variant="outlined"
+                hide-details
+                single-line
+            ></v-text-field>
+        </template>
+        <v-data-table :items="diagnoses"  :search="search"/>
+    </v-card>
 </template>
 
 <script setup>
 const diagnoses = await useFetchDiagnosis()
-
-
-
+const search = ref("")
 </script>
