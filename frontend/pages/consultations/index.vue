2ae7c86f9c4a5e7e092fe7ebc19c5d387cd4862d
Farah R <41633573+farahriz@users.noreply.github.com>
2026-04-13 01:26:48 +0800

    feat: display list of consulation notes in table on consulations page

--- frontend/pages/consultations/index.vue
+++ frontend/pages/consultations/index.vue
@@ -3,11 +3,26 @@
         <v-btn color="primary"  text="Add new consulation" prepend-icon="mdi-plus" :to="{name: 'consultations-add'}">
         </v-btn>
 
-    <v-data-table class="mt-5"></v-data-table>
+    <v-data-table class="mt-5" 
+      :items="consulations" 
+      :headers="headers">
+    </v-data-table>
 </template>
 
-<script setup lang="ts">
-import ConsultationForm from '~/components/consultationForm.vue';
+<script setup>
+const headers = [
+    {title: "ID", value: "id"},
+    {title: "Patient", value: "patient", key: "patient"},
+    {title: "Code", key: "diagnosis_id", value: item => getDiagnosisCode(item.diagnosis_id)},
+    {title: "Description", value: "desc"},
+    {title: "Created at", value: item => new Date(item.created_at).toUTCString(), key: "created_at"}
+]
+const consulations = ref(await useGetAllConsultationNotes())
+const diagnoses = ref(await useFetchDiagnosis())
 
-const consulations = ref([])
+function getDiagnosisCode(id){
+    const code = diagnoses.value.find(code => code.id == id)
+    return code.code
+}
+
 </script>
