dfe17d18a99d4cf9ca99ba4d098259b75978b741
Farah R <41633573+farahriz@users.noreply.github.com>
2026-04-12 21:01:35 +0800

    Fetch diagnosis codes from endpoint, and populate codes if none exist in the db yet

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from typing import Annotated, Optional
+import json
 
 from fastapi import FastAPI, HTTPException, Query, Depends
 from sqlmodel import Field, Session, SQLModel, create_engine, select
@@ -15,6 +16,7 @@ class Diagnosis(SQLModel, table=True):
 # 	date: str
 # 	patient: str
 
+
 #Database setup
 SQL_FILE_NAME="database.db"
 DATABASE_URL = f"sqlite:///{SQL_FILE_NAME}"
@@ -22,27 +24,51 @@ DATABASE_URL = f"sqlite:///{SQL_FILE_NAME}"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False}, echo=True)
 SQLModel.metadata.create_all(engine)
 
-# Dependency
+
 def create_db_and_tables():
 	SQLModel.metadata.create_all(engine)
+
 
 def get_session():
 	with Session(engine) as session:
 		yield session
 
-SessionDep = Annotated[Session, Depends(get_session)]
 
-# FastAPI app
+SessionDep = Annotated[Session, Depends(get_session)]
 app = FastAPI()
+
+
+def seed_db():
+	with open('code_dump.json', 'r') as dump_file:
+		code_data = json.load(dump_file)
+
+	for code_entry in code_data:
+		with Session(engine) as session:
+			new_code = Diagnosis(name=code_entry["name"], code=code_entry["code"])
+			session.add(new_code)		
+			session.commit()
+
+@app.on_event("startup")
+def on_startup():
+	create_db_and_tables()
+
 
 @app.get("/")
 def read_root():
 	return {"Hello": "World"}
 
+
 # GET list of ALL diagnosis
 @app.get("/diagnoses")
-def read_all_diagnoses():
-	codes = [{"code": "ABC", "name": "xyz"}]
+def read_all_diagnoses(
+	session: Session = Depends(get_session),
+	offset: int = 0,
+	limit: int = Query(default=200, le=200)
+) -> list[Diagnosis]:
+	codes = session.exec(select(Diagnosis).offset(offset).limit(limit)).all()
+	# hacky workaround for seeding db with codes just for the purposes of this demo
+	if(len(codes) < 1):
+		seed_db()
 	return codes
 
 
